59f90770682e3fd23d85244e00d6d787c68bcb12
sacachispasclub <129880102+sacachispasclub@users.noreply.github.com>
2023-04-04 13:40:41 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,8 +1,7 @@
-
 var users = [
-  { username: "prueba", password: "prueba" },
-  { username: "Nacho", password: "458" },
-  { username: "igna", password: "3" }
+  { username: "fede", password: "fede" },
+  { username: "facu", password: "facu" },
+  { username: "nacho", password: "nacho" }
 ];
 // Verificar si el usuario ha iniciado sesión
 if (!sessionStorage.getItem("loggedIn")) {
@@ -10,16 +9,17 @@ if (!sessionStorage.getItem("loggedIn")) {
     window.location.replace("login.html");
   }
 function login() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-
-  // Iterar a través de la lista de usuarios
-  for (var i = 0; i < users.length; i++) {
-    if (username === users[i].username && password === users[i].password) {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+  
+    for (var i = 0; i < users.length; i++) {
+      if (username === users[i].username && password === users[i].password) {
+// Si el nombre de usuario y la contraseña coinciden, iniciar sesión
       window.location.replace("index.html");
       sessionStorage.setItem("loggedIn", true);
     } else {
+      document.getElementById("message").innerHTML = "Nombre de usuario o contraseña incorrectos";
     }
 
-  } 
+  }
 }
