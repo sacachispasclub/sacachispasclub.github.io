83cfbe21026394e9c9ac1311b9759138bb3f5e1f
sacachispasclub <129880102+sacachispasclub@users.noreply.github.com>
2023-04-04 13:13:57 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,25 @@
+
+var users = [
+  { username: "nacho", password: "1" },
+  { username: "47661458", password: "458" },
+  { username: "igna", password: "3" }
+];
+// Verificar si el usuario ha iniciado sesión
+if (!sessionStorage.getItem("loggedIn")) {
+    // Si no ha iniciado sesión, redirigir a la página de inicio de sesión
+    window.location.replace("login.html");
+  }
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Iterar a través de la lista de usuarios
+  for (var i = 0; i < users.length; i++) {
+    if (username === users[i].username && password === users[i].password) {
+      window.location.replace("index.html");
+      sessionStorage.setItem("loggedIn", true);
+    } else {
+    }
+
+  } 
+}
